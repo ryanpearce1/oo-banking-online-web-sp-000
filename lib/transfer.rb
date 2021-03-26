a284df003cea21d131cca56cb56e1f10131f9cf9
Ryan Pearce <ryan.pearce@key.me>
2021-03-26 19:58:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,25 +14,19 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
+    if valid? && sender.balance > amount && self.status == "pending"
+      receiver.balance += amount
+      sender.balance -= amount
+      self.status = "complete"
+      
+    elsif
+      valid? = false 
+        puts "Transaction rejected. Please check your account balance."
+    end
+    
+      
     end
   end
-
-  def reverse_transfer
-    if @status == "complete"
-      @sender.deposit( @amount ) 
-      @receiver.deposit( @amount * -1)
-      @status = "reversed"
-    end
-  end
-
+    
 end
+  
