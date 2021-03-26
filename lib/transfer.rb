ddb84f9a3b2eabf4ff82a00775d4776b17b2ae5e
Ryan Pearce <ryan.pearce@key.me>
2021-03-26 19:56:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,20 +13,20 @@ class Transfer
     sender.valid? && receiver.valid?
   end
   
-   def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      receiver.balance += amount
+      sender.balance -= amount
+      self.status = "complete"
+      
+    elsif
+      valid? = false 
+        
+    end
+    
+      
     end
   end
-  
     
 end
   
